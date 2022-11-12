f997e235e2d5d4d6c3fd2c2f14c699bd42758283
XDQUE40 <116643129+XDQUE40@users.noreply.github.com>
2022-11-11 23:15:37 -0300

    Fds

--- source/meta/CoolUtil.hx
+++ source/meta/CoolUtil.hx
@@ -5,12 +5,13 @@ import meta.state.PlayState;
 
 using StringTools;
 
-#if sys
+
 import sys.FileSystem;
-#end
+
 
 class CoolUtil
 {
+	// tymgus45
 	public static var difficultyArray:Array<String> = ['EASY', "NORMAL", "HARD"];
 	public static var difficultyLength = difficultyArray.length;
 
@@ -63,20 +64,19 @@ class CoolUtil
 	{
 		var libraryArray:Array<String> = [];
 
-		#if sys
-		var unfilteredLibrary = FileSystem.readDirectory('$subDir/$library');
-
-		for (folder in unfilteredLibrary)
+		for (folder in Assets.list().filter(files -> files.contains('$subDir/$library')))
 		{
-			if (!folder.contains('.'))
-				libraryArray.push(folder);
+			// simulating da FileSystem.readDirectory?
+			var daFolder:String = folder.replace('$subDir/$library', '');
+			daFolder = daFolder.replace(daFolder.substring(daFolder.indexOf('/'), daFolder.length), ''); // fancy
+			if (!daFolder.startsWith('.') && !libraryArray.contains(daFolder))
+				libraryArray.push(daFolder);
 		}
-		trace(libraryArray);
-		#end
 
 		return libraryArray;
 	}
 
+	
 	public static function getAnimsFromTxt(path:String):Array<Array<String>>
 	{
 		var fullText:String = Assets.getText(path);
